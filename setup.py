a7da8b554777c295eb3dbb14c46fe09c787439cc
Leonardo Lazzaro <llazzaro@dc.uba.ar>
2015-12-19 20:28:02 -0300

    new version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class PyTest(TestCommand):
 
 setup(
     name='pystock',
-    version='0.1.4',
+    version='0.1.5',
     description='A stock market model for persistence using SQLAlchemy',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
